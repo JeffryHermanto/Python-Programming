25b45154b5ec77f0db510d5a839153f4edfc2747
Jeffry Hermanto <jeffryhermanto@gmail.com>
2021-08-17 10:46:45 +0700

    CRename folders

---
+++ 01 Getting Started/getting_started.py
@@ -0,0 +1,11 @@
+# 1. What is Python
+# 2. Installing Python
+# 3. Python Interpreter
+# 4. Code Editors
+# 5. Your first Python program
+# 6. Python extension
+# 7. Linting Python Code
+# 8. Formatting Python Code
+# 9. Running Python Code
+# 10. Python Implementations
+# 11. How Python Code is Executed
